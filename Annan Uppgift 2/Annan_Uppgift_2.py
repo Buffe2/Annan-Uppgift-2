59dee40c8337883da3054ba4ccd569a75c5a56a8
Admin <Admin@TEB123015>
2022-09-16 09:33:36 +0200

    Add project files.

---
+++ Annan Uppgift 2/Annan_Uppgift_2.py
@@ -0,0 +1,33 @@
+a = int(input("Hur manga poang fick du?"))
+if a<=100 and a>=88:
+    print("\nDina poang och betyg")
+    print("--------------------")
+    print("   P\t Betyg")
+    print(" ",a,"\t   A")
+elif a<88 and a>= 76:
+    print("\nDina poang och betyg")
+    print("--------------------")
+    print("   P\t Betyg")
+    print(" ",a,"\t   B")
+elif a<76 and a>= 64:
+    print("\nDina poang och betyg")
+    print("--------------------")
+    print("   P\t Betyg")
+    print(" ",a,"\t   C")
+elif a<64 and a>= 52:
+    print("\nDina poang och betyg")
+    print("--------------------")
+    print("   P\t Betyg")
+    print(" ",a,"\t   D")
+elif a<52 and a>= 40:
+    print("\nDina poang och betyg")
+    print("--------------------")
+    print("   P\t Betyg")
+    print(" ",a,"\t   E")
+elif a<40 and a>=0:
+    print("\nDina poang och betyg")
+    print("--------------------")
+    print("   P\t Betyg")
+    print(" ",a,"\t   F")
+else:
+    print("HAHAHAH hur kan du fa mer an 100 poang, javla fuskare.")
